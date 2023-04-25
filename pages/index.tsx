e522421bed3d72542acf0cab33211a4efe3ebe00
Lily Moon <53998902+BoringLily@users.noreply.github.com>
2023-04-25 02:09:12 -0400

    Some things work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,130 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { profile } from 'console'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import Link from 'next/link'
+import autoAnimate from '@formkit/auto-animate'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
+import type {Database} from '../utils/database.types'
 
 
 
-
-function Nav()
-{
+function Nav() {
+  const [showAuth, setShowAuth] = useState(false);
   const session = useSession();
   const supabase = useSupabaseClient();
 
 
-    function LoggedInNav()
-    {
+  return (<div className='Nav p-4 gap-6 flex flex-row justify-end w-full bg-neutral-900 mb-6' >
 
-      return(<>
-        <button onClick={()=>supabase.auth.signOut()} >Logout</button>
-      
-      </>
-        
-      )
+    {!!session && <button className='text-white p-2' onClick={() => { supabase.auth.signOut() }}>Sign Out</button>}
+    {!session && <button className='text-white p-2' onClick={() => { setShowAuth(true) }}>Login</button>}
+    {showAuth &&
+      <div className='p-4  w-screen h-screen grow flex flex-col justify-center items-center bg-black'>
+        <div className='p-4 w-full h-full flex flex-col justify-center items-center'>
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]} />
+          <button className='text-white p-2 text-lg' onClick={() => { setShowAuth(false) }}> Cancel</button>
+        </div>
+      </div>
+
     }
+  </div>)
 
-    function NotLoggedInNav()
+}
+
+
+function CreatePost() {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  const [username, setUsername] = useState("");
+  const [content, setContent] = useState("");
+
+
+  const handleCreatePost = async () => {
+    if(username.length && content.length )
     {
+      const { data, error } = await supabase
+      .from('simplepost')
+      .insert([
+        { content: content, username: username },
+      ])
 
-      return(
-        <>
-        <Link href="/">Login</Link>
-        <div>You need to login to make a post</div>
-        </> 
-      )
+        handleCanclePost();
+
     }
+  
+  }
+
+  const handleCanclePost = () => {
+    setUsername("");
+    setContent("");
+  }
+
+  if (!session) {
+    return (
+      <div className='p-4'>
 
 
-  return( <div className='Nav p-4 gap-y-6 flex rounded-full flex-row justify-center items-center w-fit bg-neutral-200' >
-    { 
-      !session? <NotLoggedInNav/>: <LoggedInNav/>}
-     </div> )
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]} />
+
+      </div>
+    )
+  }
+  else {
+
+
+    return (
+      <div className='flex flex-col justify-start gap-2 m-10'>
+
+        <div>Create a post</div>
+        <input placeholder='@username' className='min-w-fit overflow-show p-2 rounded-xl text-black resize-none ' value={username} onChange={(e) => { setUsername(e.target.value)} }/>
+        <textarea placeholder='What would you like to say?' name="post" cols={30} rows={5} className='min-w-fit overflow-show p-2 rounded-xl text-black resize-none ' value={content} onChange={(e) => { setContent(e.target.value) }}></textarea>
+
+        {content.length > 0 && <div className='flex flex-row text-white gap-4 justify-end'>
+          <button onClick={handleCanclePost}> Cancel </button> 
+          <button onClick={handleCreatePost}> Create</button>
+        </div>}
+      </div>
+
+    )
+
+
+  }
+
+
+
+
+  return <></>
 
 }
 
 
 
+function Post({content}:Database) {
+const supabase = useSupabaseClient();
+  
+  const handleDelete = async () => {
 
-function Post()
-{
+    const {error} =  await supabase.from('simplepost').delete().eq('post_id', content.post_id)
+
+  }
+
+useEffect(()=>{
+
+}, content)
 
 
-  return(
-    <div className='Post '>
-
-
-
-
-
+  return (
+    <div className='Post bg-neutral-900 p-4 flex gap-4 rounded-xl border-solid border-2 border-transparent hover:border-red-500 hover:cursor-pointer' onClick={handleDelete}>
+      <div className='text-blue-400'>@{content.username}</div>
+      <div>{content.content}</div>
     </div>
 
 
@@ -72,45 +134,62 @@ function Post()
 
 
 
-function Feed()
-{
+function Feed() {
+  const supabase = useSupabaseClient();
+  const [postData, setPostData] = useState<Database>([]);
 
-  const feedContent = <Post/>;
 
-return(
-  <div className='Feed max-w-7xl '> 
-  {feedContent}
-</div>
-)
+  
+
+  useEffect(()=>{
+
+
+
+    const getAllData = async ()=>
+    {
+      const {data, error} = await supabase.from('simplepost').select("*")
+  
+      console.log(data)
+      setPostData(data)
+    }
+    getAllData();
+
+  }, [])
+
+
+
+  const feedContent = postData.map((post:Database)=>{
+  
+    return(<Post key={post.post_id} content={post}/> )
+
+  });
+
+  return (
+    <div className='feed max-w-7xl flex flex-column gap-4  w-20'>
+      {feedContent}
+    </div>
+  )
 
 }
 
 
 
-export default function Home()
-{ 
-  const [isLoading, setLoading] = useState(true);
-  const [profile, setProfile] = useState(null);
+export default function Home() {
 
   const session = useSession()
   const supabase = useSupabaseClient()
 
+  const [createPost, setCreatePost] = useState(false);
 
-  async function getUser()
-  {
-
-      const {data, error} = await supabase.from('profile').select("*");
-
-  }
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-100">
-      
-      
-     {/* <Nav/> */}
-      <Feed/>
-      
+    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-black text-white">
+
+
+      <CreatePost />
+      <Feed />
+
 
     </main>
   )
